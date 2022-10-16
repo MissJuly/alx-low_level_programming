2377e886e5ff4263bb42369cad19cae02cf4714c
MissJuly <joygitau98@gmail.com>
2022-10-16 12:48:31 -0700

    Print possible combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,16 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
-		putchar(n);
-	if (n != 57)
+	for (n = 0; n <= 9; n++)
 	{
+		putchar((n % 10) + '0');
+		if (n == 9)
+			continue;
+
 		putchar(',');
 		putchar(' ');
 	}
+
 
 	putchar('\n');
 	return (0);
