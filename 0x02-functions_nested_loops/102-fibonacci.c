ccb67b1517c8b50653691badbb55fedfcdf0a7d1
MissJuly <joygitau98@gmail.com>
2022-10-19 20:17:40 -0700

    Print the first fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 52 fibonacci numbers
+ *
+ * Return: Nothing!
+ */
+int main(void)
+{
+	int i = 0;
+	long j = 1, g = 2;
+
+	while (i < 50)
+	{
+		if (i == 0)
+			printf("%ld", j);
+		else if (i == 1)
+			printf(", %ld", g);
+		else
+		{
+			g += j;
+			j = g - j;
+			printf(", %ld", g);
+		}
+
+		++i;
+	}
+
+	printf("\n");
+	return (0);
+}
