2538c672f427de5f013a49a70b512feffd25dd5a
MissJuly <joygitau98@gmail.com>
2022-11-03 09:49:25 -0700

    Calculate factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,15 +4,16 @@
  * factorial - Returns factorial of a given number
  * @n: the number
  *
- * Return: 1 if @n is 0,
+ * Return: -1 if @n is lower than 0,
  * or the integer value otherwise.
  */
 int factorial(int n)
 {
-	if (n == 0)
-	{
+	if (n < 0)
+		return (-1);
+
+	if (n <= 1)
 		return (1);
-	}
 
 	return (n * factorial(n - 1));
 }
