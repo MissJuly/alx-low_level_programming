4c72bc8ea2c3a495eac0dc78b13fe9f5d37934c4
MissJuly <joygitau98@gmail.com>
2022-11-15 14:20:55 +0300

    Copy 2-main.c to 02-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,11 +2,11 @@
 
 /**
  * main - function to print the name of the file it was compiled from
- * 
+ *
  * Return: 0 Always(Success)
  */
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
