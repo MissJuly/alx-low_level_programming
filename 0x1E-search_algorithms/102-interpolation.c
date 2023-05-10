a3b7aba915f2ac6333bba44f4af9ffb181f3b489
MissJuly <joygitau98@gmail.com>
2023-05-10 23:03:34 +0300

    Fix 9

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -28,7 +28,7 @@ int interpolation_search(int *array, size_t size, int value)
 
 		if (array[i] == value)
 			return (i);
-		else if (array[i] > value)
+		if (array[i] > value)
 			r = i + 1;
 		else
 			l = i + 1;
