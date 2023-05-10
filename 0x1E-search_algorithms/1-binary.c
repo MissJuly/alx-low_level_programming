fd9890f52bbd1c2de16f709a47daac13ee5a93e0
MissJuly <joygitau98@gmail.com>
2023-05-10 21:38:17 +0300

    fix fix

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -2,36 +2,38 @@
 
 /**
  * binary_search - Searches for a value in a sorted array
- *                of integers using binary search.
+ * of intergers using binary search.
  * @array: a pointer to the first element of the array to search
  * @size: the number of elements in array
  * @value: the value to search for
- *
- * Return: if the value is not present or the array is Null,
- * -1, otherwise, the index where the value is located
+ * Return: if the value is not present or the array is Null, -1,
+ * otherwise, the index where the value is located
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i, left, right, mid;
-	left = 0;
-	right = size - 1;
+	int i, left, right;
 
 	if (array == NULL)
 		return (-1);
+
+	left = 0;
+	right = size - 1;
+
 	while (left <= right)
 	{
+		int mid = left + (right - left) / 2;
+
 		printf("Searching in array: ");
 		for (i = left; i <= right; i++)
 		{
 			if (i == left)
-				printf("%d ", array[i]);
+				printf("%d", array[i]);
 			else
 				printf(", %d", array[i]);
 		}
 		printf("\n");
 
-		mid = left + (right - left) / 2;
-		if (array[mid] ==value)
+		if (array[mid] == value)
 			return (mid);
 		else if (array[mid] < value)
 			left = mid + 1;
