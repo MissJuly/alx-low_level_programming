f97df13a814ab785d4167a481a921a6ca8376cfb
MissJuly <joygitau98@gmail.com>
2023-05-10 21:35:21 +0300

    fix fix

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -12,23 +12,31 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i, left, right;
+	size_t i, left, right, mid;
+	left = 0;
+	right = size - 1;
 
 	if (array == NULL)
 		return (-1);
-
-	for (left = 0, right = size - 1; right >= left;)
+	while (left <= right)
 	{
 		printf("Searching in array: ");
-		for (i = left; i < right; i++)
-			printf("%d, ", array[i]);
-		printf("%d\n", array[i]);
+		for (i = left; i <= right; i++)
+		{
+			if (i == left)
+				printf("%d ", array[i]);
+			else
+				printf(", %d", array[i]);
+		}
+		printf("\n");
 
-		i = left + (right - left) / 2;
-		if (array[i] > value)
-			right = i - 1;
+		mid = left + (right - left) / 2;
+		if (array[mid] ==value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
 		else
-			left = i + 1;
+			right = mid - 1;
 	}
 
 	return (-1);
